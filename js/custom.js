ed6c3aa6a49a9f4a51fe227d95d5bad047a562fc
rhotep <andreas.pittrich@posteo.de>
2019-02-05 01:05:53 +0100

    misc categories#

--- js/custom.js
+++ js/custom.js
@@ -18,3 +18,25 @@ angular.module('icDirectives')
 		}
 	}
 ])
+
+
+
+angular.module('icFilters')
+
+.filter('icCategory', [
+
+	'icTaxonomy',
+
+	function(icTaxonomy){
+		var dummy = {name: 'unknown'}
+		
+		return function(item){
+			var tags = item && item.tags
+
+			if(!tags) return dummy
+
+
+			return icTaxonomy.getCategory(item && item.primaryTopic || tags)
+		}
+	}
+])
