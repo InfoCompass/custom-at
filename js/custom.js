d7ccb909faa2a4f2e021fbcd88ad2da57db41824
rhotep <andreas.pittrich@posteo.de>
2019-09-17 17:57:07 +0200

    added survey popup

--- js/custom.js
+++ js/custom.js
@@ -19,6 +19,66 @@ angular.module('icDirectives')
 	}
 ])
 
+.directive('icSurveyPopup',[
+
+	'ic',
+
+	function(ic){
+		return {
+			restrict:		'E',
+			templateUrl: 	'partials/ic-survey-popup.html',
+			scope:			true,
+
+			link: function(scope, element, attrs){
+				scope.ic = ic
+
+			}
+		}
+	}
+])
+
+.directive('icOneTimePopup',[
+
+	'ic',
+
+	function(ic){
+		return {
+			restrict:	'A',
+			scope: 		{
+							icPopupId: 		"@",
+							icPopupName: 	"@",
+							icPopupMessage: "<?"
+						},
+
+			link: function(scope, element, attrs){
+				scope.ic = ic
+
+				console.log('linking one time popup', scope.icPopupId, scope.icPopupMessage)
+
+				if(!scope.icPopupId){
+					console.warn('icOneTimePopup: missing icPopupId')
+					return null
+				}
+				var seen = {}
+
+				try{
+					seen = JSON.parse(localStorage.getItem('seenOneTimePopups')) || {}
+				} catch(e) { console.log(e) }
+
+				if(seen && seen[scope.icPopupId]) return true
+
+				ic.overlays.open(scope.icPopupName, scope.icPopupMessage || undefined)
+				.finally(function(){
+					console.log('test: overlay read', scope.icPopupId)
+					seen[scope.icPopupId] = true
+
+					localStorage.setItem('seenOneTimePopups', JSON.stringify(seen))
+				})
+			}
+		}
+	}
+])
+
 
 
 angular.module('icFilters')
